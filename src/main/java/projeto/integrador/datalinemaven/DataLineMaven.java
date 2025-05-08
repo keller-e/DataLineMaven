67c60fed51d7e547a731fe681f4be63cd0d31752
Erich Keller <erichkeller8@gmail.com>
2025-05-08 02:18:46 -0300

    Upload de projeto DateLineMaven

---
+++ src/main/java/projeto/integrador/datalinemaven/DataLineMaven.java
@@ -0,0 +1,13 @@
+package projeto.integrador.datalinemaven;
+
+import telas.telaLogin;
+
+public class DataLineMaven {
+
+    public static void main(String[] args) {
+        
+        telaLogin login = new telaLogin();
+        login.setVisible(true);
+        
+    }
+}
